9fd2d3e27cd0ee1b03708878327f71ae50f92742
mitali453 <varshneymitali812@gmail.com>
2025-03-20 23:41:13 +0530

    loader creation

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="animate-spin h-8 w-8 border-t-4 border-blue-600 rounded-full"></div>
+    </div>
+  );
+};
+
+export default Loader;
